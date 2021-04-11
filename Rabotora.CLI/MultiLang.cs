a967ea88f1db9f5fa9b0065ca091a40301652c69
Misaka12456 <a15951@qq.com>
2021-04-11 15:27:45 +0800

    Add Multi-Language Support

---
+++ Rabotora.CLI/MultiLang.cs
@@ -0,0 +1,101 @@
+﻿using Rabotora.MultiLanguage;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabotora.CLI
+{
+	/// <summary>
+	/// Rabotora CLI多语言支持类
+	/// </summary>
+	public static class MultiLang
+	{
+		/// <summary>
+		/// 获取指定id和指定区域 <see cref="CultureInfo"/> 对应的本地化资源。
+		/// </summary>
+		/// <typeparam name="T">资源的类型。</typeparam>
+		/// <param name="id">资源id。</param>
+		/// <param name="info">区域的 <see cref="CultureInfo"/> 类实例。</param>
+		/// <returns>成功返回本地化的资源; 存在该资源但不存在该区域对应的本地化资源返回en-US(英文)本地化的资源; 失败返回 <see langword="null"/> 或指定值类型 <typeparamref name="T"/> 的默认值。</returns>
+		public static T? GetRes<T>(string id, CultureInfo info)
+		{
+			if (string.IsNullOrWhiteSpace("id"))
+			{
+				return default;
+			}
+			else if (typeof(T) == typeof(string))
+			{
+				var multiLang = Assembly.LoadFrom("Rabotora.MultiLanguage.dll");
+				var type = multiLang.GetType($"Rabotora.MultiLanguage.{info.Name.Replace("-", "_")}");
+				if (type != null)
+				{
+					string? r = ((ResourceManager)type.GetProperty("ResourceManager")!.GetGetMethod()!.Invoke(null, null)!).GetString(id);
+					if (r != null)
+					{
+						return (T?)Convert.ChangeType(r.Replace("\\t","\t"), typeof(T));
+					}
+					else
+					{
+						string? r2 = en_US.ResourceManager.GetString(id);
+						if (r2 != null)
+						{
+							return (T?)Convert.ChangeType(r2.Replace("\\t", "\t"), typeof(T));
+						}
+						else
+						{
+							return default;
+						}
+					}
+				}
+				else
+				{
+					string? r2 = en_US.ResourceManager.GetString(id);
+					if (r2 != null)
+					{
+						return (T?)Convert.ChangeType(r2.Replace("\\t", "\t"), typeof(T));
+					}
+					else
+					{
+						return default;
+					}
+				}
+			}
+			else
+			{
+				var type = Type.GetType($"Rabotora.MultiLanguage.{info.Name.Replace("-", "_")}");
+				if (type != null)
+				{
+					object? r = ((ResourceManager)type.GetProperty("ResourceManager")!.GetGetMethod()!.Invoke(null, null)!).GetObject(id);
+					if (r != null)
+					{
+						return (T?)r;
+					}
+					else
+					{
+						return (T?)en_US.ResourceManager.GetObject(id);
+					}
+				}
+				else
+				{
+					return (T?)en_US.ResourceManager.GetObject(id);
+				}
+			}
+		}
+
+		/// <summary>
+		/// 获取指定id和系统当前区域 <see cref="CultureInfo"/> 对应的本地化资源。
+		/// </summary>
+		/// <typeparam name="T">资源的类型。</typeparam>
+		/// <param name="id">资源id。</param>
+		/// <returns>成功返回本地化的资源; 存在该资源但不存在对应的本地化资源返回en-US(英文)本地化的资源; 失败返回 <see langword="null"/> 或指定值类型 <typeparamref name="T"/> 的默认值。</returns>
+		public static T? GetRes<T>(string id)
+		{
+			return GetRes<T>(id, CultureInfo.InstalledUICulture);
+		}
+	}
+}
--- Rabotora.CLI/Program.cs
+++ Rabotora.CLI/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.Reflection;
+using static Rabotora.CLI.MultiLang;
 
 namespace Rabotora.CLI
 {
@@ -6,6 +9,39 @@ namespace Rabotora.CLI
 	{
 		static void Main(string[] args)
 		{
+			switch (args.Length)
+			{
+				case 0:
+					Console.WriteLine(GetRes<string>("CLI.BaseHelp"));
+					break;
+				case 1:
+					if (args[0].Trim().StartsWith('-'))
+					{
+						string option = args[0].Trim().ToLower()[1..];
+						switch (option)
+						{
+							case "ver":
+							case "-version":
+								Console.WriteLine(string.Format(GetRes<string>("CLI.Version")!,
+									Assembly.GetExecutingAssembly()!.GetName().Version!.ToString(),
+									Assembly.GetEntryAssembly()!.GetName().Version!.ToString()));
+								break;
+							case "?":
+								Console.WriteLine(GetRes<string>("CLI.Help"));
+								break;
+							default:
+								Console.WriteLine(GetRes<string>("CLI.Help"));
+								break;
+						}
+					}
+					else if (args[0].Trim().ToLower().EndsWith(".rexe"))
+					{
+						Console.WriteLine("Rabotora Runtime is still developing, please wait for patience...");
+					}
+					break;
+				default:
+					break;
+			}
 		}
 	}
 }
